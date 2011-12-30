5b2bc93efd9d01bba1e97fdbef36af06c13f9860
Damien Lesgourgues <damien@lesgourgues.fr>
2011-12-30 16:52:36 +0100

    Adding licence informations

--- controller.tcl
+++ controller.tcl
@@ -1,4 +1,25 @@
 #!/usr/bin/tclsh
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the Licence, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# Product name :  TCL GameServer
+# Copyright (C) 2011 Damien Lesgourgues
+# Author(s): Damien Lesgourgues
+#
+##############################################################################
 
 proc socket_control {sock} {
   global mysock
--- uno.tcl
+++ uno.tcl
@@ -1,6 +1,26 @@
 #!/usr/bin/tclsh
+##############################################################################
 #
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the Licence, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# Product name :  TCL GameServer
+# Copyright (C) 2011 Damien Lesgourgues
+# Author(s): Damien Lesgourgues
 # Based on UNO bot by Marky v0.96
+#
+##############################################################################
 #
 # Marky's Uno v0.96
 # Copyright (C) 2004 Mark A. Day (techwhiz@earthlink.net)
@@ -21,6 +41,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
+##############################################################################
 
 # Parametres pour le jeu UNO
 set mysock(uno-nick) "UNO"
