c141b669ee5ab35e4ce81c582f22e3083cfb7e52
Damien Lesgourgues <damien@lesgourgues.fr>
2011-12-30 05:05:07 +0100

    correction config bot uno

--- uno.tcl
+++ uno.tcl
@@ -1,12 +1,11 @@
 #!/usr/bin/tclsh
 
 # Parametres pour le jeu UNO
-set mysock(uno)(nick) "UNO"
-set mysock(uno)(username) "uno"
-set mysock(uno)(hostname) "uno.$mysock(hostname)"
-set mysock(uno)(realname) "Bot de jeu UNO"
-set mysock(uno)(chan) "#UNO"
+set mysock(uno-nick) "UNO"
+set mysock(uno-username) "uno"
+set mysock(uno-hostname) "uno.$mysock(hostname)"
+set mysock(uno-realname) "Bot de jeu UNO"
+set mysock(uno-chan) "#UNO"
+lappend mysock(gamelist) "uno"
+nodouble $mysock(gamelist)
 
-# Initialisation du bot
-bot_init $mysock(uno)(nick) $mysock(uno)(username) $mysock(uno)(hostname) $mysock(uno)(realname)
-join_chan $mysock(uno)(chan)
