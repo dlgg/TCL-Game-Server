f1274699c0cc0a68c603e71f353267f652e8e125
Damien Lesgourgues <damien@lesgourgues.fr>
2012-01-13 14:28:21 +0100

    debug of init script

--- gameserver.sh
+++ gameserver.sh
@@ -14,62 +14,52 @@ fi
 
 # Check if TCL GameService is running. 0 for yes / 1 for no.
 testprocess() {
-  if [z$pid == "znone"]; then return 1; fi
+  if [ z$pid == "znone" ]; then return 1; fi
   ps x | grep -v grep | grep $pid >/dev/null 2>&1
   return $?
 }
 
 startprocess() {
-  if [! -x $tclsh]; then
+  if [ ! -x $tclsh ]; then
     echo "ERROR : tclsh is not found. Exiting." >&2
     exit 1
   fi
   if testprocess; then
     echo "TCL Game Service is already running." >&2
   else
-    nohup $tclsh ./main.tcl >$logfile 2>&1
+    nohup $tclsh ./main.tcl >$logfile 2>&1 &
   fi
 }
 
 stopprocess() {
-  if [z$pid == "znone"]; then
+  if [ z$pid == "znone" ]; then
     echo "TCL Game Service is not started or has been started by an bad way." >&2
   else
-    kill -15 $pid
-  fi
-  echo "Check if TCL Game Service is stoped"
-  sleep 2
-  if testprocess; then
-    echo "TCL Game Service seem to be always running. Consider to use $0 forcestop." >&2
-  fi
-}
-
-forceprocess() {
-  if [z$pid == "znone"]; then
-    echo "TCL Game Service is not started or has been started by an bad way." >&2
-  else
-    kill -9 $pid
-  fi
-  echo "Check if TCL Game Service is stoped"
-  sleep 2
-  if testprocess; then
-    echo "TCL Game Service seem to be always running. There is a big problem ... :S" >&2
+    kill -$1 $pid
+    sleep 2
+    echo "Check if TCL Game Service is stoped"
+    if testprocess; then
+      echo "TCL Game Service seem to be always running. Consider to use $0 forcestop." >&2
+    else
+      rm $pidfile
+    fi
   fi
 }
 
 case "$1" in
   start)
-    log "Starting TCL Game Service"
-    startprocess normal
+    echo "Starting TCL Game Service"
+    startprocess
     ;;
 
   stop)
-    log "Stopping TCL Game Service"
-    stopprocess
+    echo "Stopping TCL Game Service"
+    stopprocess 15
     ;;
 
   forcestop)
-    forceprocess
+    echo "Force stopping of TCL Game Service"
+    stopprocess 9
     ;;
 
   restart)
